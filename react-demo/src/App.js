27bc646dbacdb0c76fd389bd9a4a4c181bfa2424
mvn-loantruong-dn <loan.truong@monstar-lab.com>
2021-06-26 14:55:21 +0700

    Ex3 Form and Lifting State Up

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -1,18 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
 import Footer from './Footer';
+import Form from './Form';
 
 function App() {
   return (
     <>
     <Header></Header>
-    <main class="page-main flex-centered">
-      <div class="container">
-        <div class="content">
-        <a href="#"><img src="./logo512.png" alt="Logo" /></a>
-        <h3>Hello React !!!</h3>
-        </div>
+    <main className="page-main flex-centered">
+      <div className="container">
+        <Form />
       </div>
     </main>
     <Footer></Footer>
